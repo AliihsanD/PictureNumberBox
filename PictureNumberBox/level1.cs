cca1b4777478b00c99c02d53e47b9f573d2d30db
AliihsanD <bursasporali88@gmail.com>
2023-04-25 14:41:56 +0300

    yorum satıları eklendi proje kopmle bitti.

--- PictureNumberBox/level1.cs
+++ PictureNumberBox/level1.cs
@@ -18,43 +18,43 @@ namespace PictureNumberBox
             
         }
 
-        PictureBox[] pictures;
-        PictureBox[] pictures2;
+        PictureBox[] pictures; // level1
+        PictureBox[] pictures2; // level2
 
-        Point[] firstLocations = new Point[9];
-        Point[] firstLocations2 = new Point[16];
+        Point[] firstLocations = new Point[9]; // level1 lokasyonu
+        Point[] firstLocations2 = new Point[16]; // level2 lokasyonu
 
         int sayac = 0;
-        bool level2Aktif = false;
+        bool level2Aktif = false; // level2 açık kapalı olması
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //level1 resimlerin gruplanması
             pictures = new PictureBox[] 
             { 
                 pbSolUst, pbOrtaUst, pbSagUst, pbSolOrta, pbOrtaOrta, pbSagOrta, pbSolAlt, pbOrtaAlt, pbSolAlt,pbBos 
             };
-            pbBos.Visible = false;
+            pbBos.Visible = false; //level1 boş olan resim
+
+            //level1 resimlere tıklamayı pasif etme
             foreach (var picture in pictures)
             {
                 picture.Enabled = false;
             }
+
+            //level1 resimlerin ilk lokasyonunu kayıt etme
             for (int i = 0; i < 9; i++)
             {
                 firstLocations[i] = pictures[i].Location;
             }
 
+            //level2 resimleri gruplanması
             pictures2 = new PictureBox[]
             {
                 pbA1,pbA2,pbA3,pbA4,pbB1,pbB2,pbB3,pbB4,pbC1,pbC2,pbC3,pbC4,pbD1,pbD2,pbD3,pbD4
             };
-            foreach (var picture in pictures)
-            {
-                picture.Enabled = false;
-            }
-            for (int i = 0; i < 9; i++)
-            {
-                firstLocations[i] = pictures[i].Location;
-            }
+            
+            //level2 resimlerin ilk konumlarını kayıt etme
             for (int i = 0; i < 16; i++)
             {
                 firstLocations2[i] = pictures2[i].Location;
@@ -67,12 +67,13 @@ namespace PictureNumberBox
             Random random = new Random();
             if (!level2Aktif)
             {
-                pbBos.Visible = false;
-
+                //level1 resimlere tıklamayı aktif etme
                 foreach (var picture in pictures)
                 {
                     picture.Enabled = true;
                 }
+
+                //level1 resimleri karıştırma
                 for (int i = 0; i < 1000; i++)
                 {
                     int pictureNo = random.Next(8);
@@ -83,8 +84,9 @@ namespace PictureNumberBox
             }
             else
             {
-                pbD4.Visible = false;
+                pbD4.Visible = false; // level2 boş olan resim
 
+                //level2 resimleri karıştırma
                 for (int i = 0; i < 1000; i++)
                 {
                     int pictureNo = random.Next(15);
@@ -99,54 +101,62 @@ namespace PictureNumberBox
 
         public void pbSolUst_Click(object sender, EventArgs e)
         {
-            movePicture((PictureBox)sender);
+            movePicture((PictureBox)sender); //tıklanılan resmi hareket ettirme metodu
 
             int temp = 0;
+
+            //level1 lokasyon doğrulama
             if (!level2Aktif)
             {
                 for (int i = 0; i < 9; i++)
                 {
+                    //level1 birer birer lokasyon kontrolü
                     if (firstLocations[i] == pictures[i].Location)
                     {
                         temp++;
                     }
-                    if (temp==9)
+                    if (temp==9) //level1 lokasyonların hepsi doğru konumdaysa girilir
                     {
+                        //level1 resimlere tıklamanın kapanması
                         foreach (var picture in pictures)
                         {
                         picture.Enabled = false;
                         }
-                        pbBos.Visible = true;
-                        lblNextLevel.Visible = true;
+                        pbBos.Visible = true; //boş olan resmin gözükmesi
+                        lblNextLevel.Visible = true; //2.seviye  butonunun görünmesi
                         MessageBox.Show("Tebrikler Skorunuz : " + lblSkorNumber.Text);    
                     }
                 }
             }
             else
             {
+                //level2 lokasyon kontrolü
                 for (int i = 0; i < 16; i++)
                 {
+                    //level2 birer birer lokasyon kontrolü
                     if (firstLocations2[i] == pictures2[i].Location)
                     {
                         temp++;
                     }
-                    if (temp == 16)
+                    if (temp == 16) //level2 lokasyonların hepsi doğru konumdaysa girilir
                     {
+                        //level2 resimlerin tıklmasının kapatılması
                         foreach (var picture in pictures2)
                         {
                             picture.Enabled = false;
                         }
-                        pbD4.Visible = true;
+                        pbD4.Visible = true; //level2 boş olan resmin gözükmesi
                         MessageBox.Show("Tebrikler Skorunuz : " + lblSkorNumber.Text);
                     }
                 }
             }
         }
         
-        public void movePicture(PictureBox picture)
+        public void movePicture(PictureBox picture) //resimleri hareket ettirme
         {
             if (!level2Aktif)
             {
+                //level1 tıklanılan resmin dört bir yanınında boş bölme kontrolü
                 if (
                 picture.Left + 120 == pbBos.Left && picture.Top == pbBos.Top ||
                 picture.Left - 120 == pbBos.Left && picture.Top == pbBos.Top ||
@@ -154,8 +164,9 @@ namespace PictureNumberBox
                 picture.Left == pbBos.Left && picture.Top + 120 == pbBos.Top
                )
                 {
-                    Point temp = picture.Location;
-                    picture.Location = pbBos.Location;
+                    //geçiçi lokasyon aktarımı
+                    Point temp = picture.Location;      
+                    picture.Location = pbBos.Location;  
                     pbBos.Location = temp;
                     sayac++;
                     lblSkorNumber.Text = sayac.ToString();
@@ -163,6 +174,7 @@ namespace PictureNumberBox
             }
             else
             {
+                //level2 tıklanılan resmin dört bir yanınında boş bölme kontrolü
                 if (
                 picture.Left + 90 == pbD4.Left && picture.Top == pbD4.Top ||
                 picture.Left - 90 == pbD4.Left && picture.Top == pbD4.Top ||
@@ -170,6 +182,7 @@ namespace PictureNumberBox
                 picture.Left == pbD4.Left && picture.Top + 90 == pbD4.Top
                )
                 {
+                    //geçiçi lokasyon aktarımı
                     Point temp = picture.Location;
                     picture.Location = pbD4.Location;
                     pbD4.Location = temp;
@@ -181,22 +194,27 @@ namespace PictureNumberBox
 
         private void lblNextLevel_Click(object sender, EventArgs e)
         {
-            level2Aktif = true;
+            level2Aktif = true; //level2 butonu aktif
             btnStartRestart.Text = "Başla";
+            
+            //level1 resimleri kapanır
             foreach (var picture in pictures)
             {
                 picture.Visible = false;
             }
+
+            //level2 resimleri açılır
             foreach (var picture in pictures2)
             {
                 picture.Visible = true;
             }
-            lblNextLevel.Visible = false;
+            lblNextLevel.Visible = false; //level2 butonu gizlenir
         }
 
+        //level2 butonu stilleştirme
         private void lblNextLevel_MouseHover(object sender, EventArgs e)
         {
-            lblNextLevel.ForeColor = Color.Green;
+            lblNextLevel.ForeColor = Color.Green; 
         }
 
         private void lblNextLevel_MouseLeave(object sender, EventArgs e)
